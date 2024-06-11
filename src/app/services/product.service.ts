a016c3451acadc5340fab0c850d2b151b9b5c79f
talash-naeem <naeemtalash153@gmail.com>
2024-06-11 11:18:37 +0500

    add section-14

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,14 +22,23 @@ export class ProductService {
    
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    
+    return this.getProducts(searchUrl);
   }
   getProductCategories(): Observable<ProductCategory[]>{
 
     return this.httpClient.get<GetResponseProductsCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
+    );
+  }
+  searchProducts(theKeyword: string): Observable<Product[]>{
+    const searchUrl = `${this.baseUrl}/search/findByContaining?name=${theKeyword}`;
+    
+    return this.getProducts(searchUrl);
+  }
+  private getProducts(searchUrl: string): Observable<Product[]>{
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
     );
   }
 }
