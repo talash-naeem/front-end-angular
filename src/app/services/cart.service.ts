bf7f2e9f683f52d839fe383bef34b886ff6de43d
talash-naeem <naeemtalash153@gmail.com>
2024-06-25 17:09:28 +0500

    add section 22

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { CartItem } from '../common/cart-item';
 
 @Injectable({
@@ -9,8 +9,8 @@ import { CartItem } from '../common/cart-item';
 export class CartService {
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
 
   constructor() { }
